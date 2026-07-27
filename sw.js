26b309164e7630699c7df3ab3a9112136af569e2
bonaqu <48132049+bonaqu@users.noreply.github.com>
2026-07-27 18:34:30 +0300

    fix automatic PWA cache invalidation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'nte-voice-v1.2.8';
+const CACHE_VERSION = 'nte-voice-__STATIC_CACHE_FINGERPRINT__';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DATA_CACHE = `${CACHE_VERSION}-data`;
 const STATIC_ASSETS = [
